8bf058362d2dce915cd1bfe6dd56b63ae7dba0a4
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-26 01:10:44 +0530

    set auth basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Router, Switch, Route, Redirect } from 'react-router-dom'
+import Interviews from './pages/Interviews';
+import Error404 from './pages/Error404';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import history from './history';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/' component={Interviews} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Redirect to='/not_found' component={Error404} />
+      </Switch>
+    </Router>
   );
 }
 
