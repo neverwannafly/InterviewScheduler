75030b8419411c448588fa2de0a2385bd7bdb046
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-27 01:59:20 +0530

    add method to retrieve events

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -6,13 +6,31 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import '@fullcalendar/core/main.min.css';
 import '@fullcalendar/daygrid/main.min.css';
 import '@fullcalendar/timegrid/main.min.css';
+import { SERVER_PREFIX } from '../config';
+import attachHeaders from '../utils/attachHeaders';
 
-const Calendar = () => {
+const Calendar = ({userData}) => {
   useEffect(() => {
-    
     const calendarEl = document.getElementById('calendar-root');
+    const baseUrl = `${SERVER_PREFIX}/api/interviews/fetch`;
     const calendar = new FullCalendar(calendarEl, {
       plugins: [timeGridPlugin],
+      events: {
+        url: attachHeaders(baseUrl, userData),
+      },
+      eventClick: async info => {
+        const interviewId = info.event.id;
+        const baseUrl = `${SERVER_PREFIX}/api/interviews/get/${interviewId}`;
+        const response = await fetch(attachHeaders(baseUrl, userData), {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+
+        const data = await response.json();
+        console.log(data);
+      }
     });
     calendar.render();
   });
