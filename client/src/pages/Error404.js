8a2449814265ec83bc23a8d5e71ca4ff2cdcfdb4
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-27 02:38:33 +0530

    customize error page

--- client/src/pages/Error404.js
+++ client/src/pages/Error404.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
-const Error404 = () => {
+const Error404 = ({user, loading, errors}) => {
   return (
-    <div className="wrapper">
-      Oops something bad happened!
+    <div className="page">
+      <Navbar userData={user} />
+      <div className="wrapper">
+        <Header name="ERROR - 404"/>
+        <div className="center">
+          <h2>The page you're looking for doesnt exist!</h2>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Error404;
+const mapStateToProps = state => ({
+  user: state.auth.user,
+  loading: state.auth.loading,
+  errors: state.auth.errors,
+});
+
+export default connect(mapStateToProps)(Error404);
