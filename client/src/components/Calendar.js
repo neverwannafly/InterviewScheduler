9324f6200d37abba4b4cf646780dd4b712cb45a9
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-27 19:32:58 +0530

    view interview details on event click

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Calendar as FullCalendar } from '@fullcalendar/core';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -8,8 +8,15 @@ import '@fullcalendar/daygrid/main.min.css';
 import '@fullcalendar/timegrid/main.min.css';
 import { SERVER_PREFIX } from '../config';
 import attachHeaders from '../utils/attachHeaders';
+import ViewInterview from './ViewInterview';
+import {Modal} from 'react-bootstrap';
 
 const Calendar = ({userData}) => {
+  const [show, setShow] = useState(false);
+  const [interview, setInterview] = useState({});
+
+  const handleModalClose = () => setShow(false);
+
   useEffect(() => {
     const calendarEl = document.getElementById('calendar-root');
     const baseUrl = `${SERVER_PREFIX}/api/interviews/fetch`;
@@ -28,14 +35,34 @@ const Calendar = ({userData}) => {
           }
         });
         const data = await response.json();
+        setInterview(data); setShow(true);
         console.log(data);
       }
     });
     calendar.render();
   }, [userData]);
   return (
-    <div id="calendar-root">
-      
+    <div className="component">
+      <Modal show={show} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ViewInterview interview={interview} />
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-outline-primary btn-inline" onClick={handleModalClose}>
+            Close
+          </button>
+          <button className="btn btn-outline-success btn-inline" onClick={handleModalClose}>
+            Edit
+          </button>
+          <button className="btn btn-outline-danger btn-inline" onClick={handleModalClose}>
+            Delete
+          </button>
+        </Modal.Footer>
+      </Modal>
+      <div id="calendar-root"></div>
     </div>
   )
 }
