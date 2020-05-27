0b577d9eba9e9b45a1cf6bb5769536d975376680
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-27 14:31:52 +0530

    add keys to rows and cells

--- client/src/components/UserInterviewRow.js
+++ client/src/components/UserInterviewRow.js
@@ -3,26 +3,26 @@ import React from 'react';
 const UserInterviewRow = ({isHeading, content}) => {
   if (isHeading) {
     return (
-      <tr>
-        <th className="user-interview-cell" scope="col"> ID </th>
-        <th className="user-interview-cell" scope="col"> Title </th>
-        <th className="user-interview-cell" scope="col"> Agenda </th>
-        <th className="user-interview-cell" scope="col"> Start Time </th>
-        <th className="user-interview-cell" scope="col"> End Time </th>
-        <th className="user-interview-cell" scope="col"> Comments </th>
-        <th className="user-interview-cell" scope="col"> Created By </th>
+      <tr key='0'>
+        <th key='0' className="user-interview-cell" scope="col"> ID </th>
+        <th key='1' className="user-interview-cell" scope="col"> Title </th>
+        <th key='2' className="user-interview-cell" scope="col"> Agenda </th>
+        <th key='3' className="user-interview-cell" scope="col"> Start Time </th>
+        <th key='4' className="user-interview-cell" scope="col"> End Time </th>
+        <th key='5' className="user-interview-cell" scope="col"> Comments </th>
+        <th key='6' className="user-interview-cell" scope="col"> Created By </th>
       </tr>
     );
   } else {
     return (
-      <tr>
-        <td className="user-interview-cell"> {content.id} </td>
-        <td className="user-interview-cell"> {content.title} </td>
-        <td className="user-interview-cell"> {content.agenda} </td>
-        <td className="user-interview-cell"> {content.start} </td>
-        <td className="user-interview-cell"> {content.end} </td>
-        <td className="user-interview-cell"> {content.comments} </td>
-        <td className="user-interview-cell"> {content.createdBy} </td>
+      <tr key={content.id}>
+        <td key='0' className="user-interview-cell"> {content.id} </td>
+        <td key='1' className="user-interview-cell"> {content.title} </td>
+        <td key='2' className="user-interview-cell"> {content.agenda} </td>
+        <td key='3' className="user-interview-cell"> {content.start} </td>
+        <td key='4' className="user-interview-cell"> {content.end} </td>
+        <td key='5' className="user-interview-cell"> {content.comments} </td>
+        <td key='6' className="user-interview-cell"> {content.created_by} </td>
       </tr>
     );
   }
