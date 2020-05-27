ba645b8f35ec467d4c8cecff47f65c08e91c5965
Shubham Anand <neverwannafly@Shubhams-MacBook-Pro.local>
2020-05-27 14:32:06 +0530

    display interviews tab;e

--- client/src/pages/UserInterview.js
+++ client/src/pages/UserInterview.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Navbar from '../components/Navbar';
 import UserInterviewRow from '../components/UserInterviewRow';
+import { SERVER_PREFIX } from '../config';
+import attachHeaders from '../utils/attachHeaders';
 
 const UserInterview = ({user, loading, errors}) => {
+  let [interviews, setInterviews] = useState([]);
+
+  useEffect(() => {
+    let didCancel = false;
+    const url = `${SERVER_PREFIX}/interviews/${user.id}`;
+    fetch(attachHeaders(url, user)).then(response => 
+      (response.json())
+    ).then(data => {
+      if (!didCancel) setInterviews(data.interviews);
+    });
+    return () => {didCancel = true;}
+  }, [user]);
+
+  console.log(interviews);
+
   return (
     <div className="page">
       <Navbar userData={user} />
@@ -14,6 +31,10 @@ const UserInterview = ({user, loading, errors}) => {
               <UserInterviewRow isHeading={true} />
             </thead>
             <tbody id="table-content-root">
+              { interviews.map(interview => UserInterviewRow({
+                isHeading: false,
+                content: interview,
+              })) }
             </tbody>
           </table>
         </div>
